a778cc299be858c7edd0274b876377af13a755ba
Rob Hague <rob@rho.org.uk>
2023-08-13 12:55:12 +0100

    Use GP2 and GP3 for display on tiny2040

--- code.py
+++ code.py
@@ -22,7 +22,7 @@ print(f"Morse setup time: {end-start}ms")
 # Identify device-specific pins
 if "tiny2040" in board.board_id:
     print("Tiny2040 Config")
-    display_sda, display_scl = board.GP0, board.GP1
+    display_sda, display_scl = board.GP2, board.GP3
     neopixel_pin = board.GP28
     key_pin = board.GP27
 
